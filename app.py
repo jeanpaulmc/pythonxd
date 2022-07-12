836d0dcd3cbd9472ef4887411dca55579041b9e7
Jean Paul <jean.melendez@utec.edu.pe>
2022-07-12 16:15:51 -0500

    test

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ class Postas():
     def __init__(self):
         self.total_centro_vacunacion = 0
 
+
 class Usuario():
     def __init__(self, username):
         self.username = username
         self.alta = False
+
 
 class CentroVacunacion():
     def __init__(self):
@@ -13,34 +15,45 @@ class CentroVacunacion():
         self.total_vacunados_1 = 0
         self.total_vacunados_2 = 0
 
-    def personas_vacunadas_1(self,personas):
+    def personas_vacunadas_1(self, personas):
         self.total_vacunados_1 += personas
-    
-    def personas_vacunadas_2(self,personas):
+
+    def personas_vacunadas_2(self, personas):
         self.total_vacunados_2 += personas
 
-    def porc_personas(self,anios):
+    def porc_personas_8(self, anios):
         if anios >= 80:
             self.total_vacunados_1 -= 5000
             return (5000/647355)*100
-        elif anios>=70 and anios<=79:
-            self.total_vacunados_1 -= 3000
-            self.total_vacunados_2 -= 3000
-            return (3000/1271842)*100
-        elif anios>=60 and anios<=69:
-            self.total_vacunados_1 -= 5000
-            return (5000/2221241)*100
-        elif anios>=50 and anios<=59:
-            self.total_vacunados_1 -= 5000
-            return (5000/3277134)*100
-        elif anios>=40 and anios<=49:
-            self.total_vacunados_1 -= 5000
-            return (5000/4183174)*100
-        elif anios>=30 and anios<=39:
-            self.total_vacunados_1 -= 5000
-            return (5000/5031117)*100
-        elif anios>=18 and anios<=29:
-            self.total_vacunados_1 -= 5000
-            return (5000/6303670)*100
         else:
             return -1
+
+    def porc_personas_70(self, anios):
+        if anios >= 70 and anios <= 79:
+            self.total_vacunados_1 -= 3000
+            self.total_vacunados_2 -= 3000
+
+    def porc_personas_60(self, anios):
+        if anios >= 60 and anios <= 69:
+            self.total_vacunados_1 -= 5000
+            return (5000/2221241)*100
+
+    def porc_personas_50(self, anios):
+        if anios >= 50 and anios <= 59:
+            self.total_vacunados_1 -= 5000
+            return (5000/3277134)*100
+
+    def porc_personas_40(self, anios):
+        if anios >= 40 and anios <= 49:
+            self.total_vacunados_1 -= 5000
+            return (5000/4183174)*100
+
+    def porc_personas_30(self, anios):
+        if anios >= 30 and anios <= 39:
+            self.total_vacunados_1 -= 5000
+            return (5000/5031117)*100
+
+    def porc_personas_20(self, anios):
+        if anios >= 18 and anios <= 29:
+            self.total_vacunados_1 -= 5000
+            return (5000/6303670)*100
